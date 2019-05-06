6bd77610465f2f6bcdf9f6ac6968bc97c8791f5e
darldil <mabbati@ucm.es>
2019-05-06 11:54:28 +0200

    Servidor 1.0 - Build 26
    Añadida licencia MIT.
    Mejoras en el control de excepciones.

--- Servidor/JanetServJarvis.py
+++ Servidor/JanetServJarvis.py
@@ -1,13 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 Servidor de TFG - Proyecto Janet
-Versión 0.9.0
+Versión 1.0
 
-@author: Mauricio Abbati Loureiro - Jose Luis Moreno Varillas
-© 2018-2019 Mauricio Abbati Loureiro - Jose Luis Moreno Varillas. All rights reserved.
+MIT License
+
+Copyright (c) 2019 Mauricio Abbati Loureiro - Jose Luis Moreno Varillas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-from urllib import request, parse
+from urllib import request, parse, error
 import json
 
 class JanetServJarvis():
@@ -17,14 +33,18 @@ class JanetServJarvis():
         contenido = contenido[0].lower() + contenido[1:]
         data = {'user_id': id, 'content': contenido}
 
-        req = request.Request("http://www.janetbiblio.tk:5000", data=parse.urlencode(data).encode())
-
-        resp = request.urlopen(req)
+        try:
+            req = request.Request("http://localhost:5000", data=parse.urlencode(data).encode())
+            resp = request.urlopen(req)
+        except error.URLError as e:
+            msg = "Janet se encuentra en mantenimiento en estos momentos. " \
+                      "Inténtelo de nuevo más tarde"
+            raise error.HTTPError("http://localhost:5000", 400, msg, None, None)
 
         return json.load(resp)
 
     def restart(self, id):
         data = {'user_id': id, 'content': '/restart'}
 
-        req = request.Request("http://www.janetbiblio.tk:5000", data=parse.urlencode(data).encode())
+        req = request.Request("http://localhost:5000", data=parse.urlencode(data).encode())
         request.urlopen(req)
