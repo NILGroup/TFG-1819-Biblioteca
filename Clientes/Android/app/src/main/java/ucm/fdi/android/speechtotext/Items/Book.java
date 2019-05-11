bb9dc3b8bb6a4da2a552e52c7506251ac023e801
Jose Luis Moreno <josse995@gmail.com>
2019-05-11 21:50:34 +0200

    Update license

--- Clientes/Android/app/src/main/java/ucm/fdi/android/speechtotext/Items/Book.java
+++ Clientes/Android/app/src/main/java/ucm/fdi/android/speechtotext/Items/Book.java
@@ -1,9 +1,10 @@
 
 /*
  * Book.java
- * Created by Jose Luis Moreno on 5/5/19 1:15 PM
- * Copyright (c) 2019 . All rights reserved.
- * Last modified 5/5/19 12:53 PM
+ * Created by Jose Luis Moreno on 5/11/19 9:49 PM
+ * MIT License
+ * Copyright (c) 2019 . Mauricio Abbati Loureiro - Jose Luis Moreno Varillas
+ * Last modified 5/5/19 1:15 PM
  */
 
 package ucm.fdi.android.speechtotext.Items;
--- Clientes/Android/app/src/main/java/ucm/fdi/android/speechtotext/Items/Phone.java
+++ Clientes/Android/app/src/main/java/ucm/fdi/android/speechtotext/Items/Phone.java
@@ -1,8 +1,9 @@
 /*
  * Phone.java
- * Created by Jose Luis Moreno on 5/5/19 1:15 PM
- * Copyright (c) 2019 . All rights reserved.
- * Last modified 5/5/19 12:55 PM
+ * Created by Jose Luis Moreno on 5/11/19 9:49 PM
+ * MIT License
+ * Copyright (c) 2019 . Mauricio Abbati Loureiro - Jose Luis Moreno Varillas
+ * Last modified 5/5/19 1:15 PM
  */
 
 package ucm.fdi.android.speechtotext.Items;
